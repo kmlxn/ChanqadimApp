9c98686ec58a10aa5c2a011cdedb4e039be93c37
Komilxon <komilkhon.asamov@hotmail.com>
2017-05-13 19:06:48 +0500

    Update react native to 0.44.0

--- android/app/src/main/java/com/chanqadimv3/MainApplication.java
+++ android/app/src/main/java/com/chanqadimv3/MainApplication.java
@@ -1,11 +1,10 @@
 package com.chanqadimv3;
 
 import android.app.Application;
-import android.util.Log;
 
 import com.facebook.react.ReactApplication;
 import com.imagepicker.ImagePickerPackage;
-import com.facebook.react.ReactInstanceManager;
+//import com.facebook.react.ReactInstanceManager; //
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -18,7 +17,7 @@ public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
-    protected boolean getUseDeveloperSupport() {
+    public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
     }
 
