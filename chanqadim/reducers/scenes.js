ee6807b19c1baf79dbbf5c3c72874c5d86347b94
Komilxon <komilkhon.asamov@hotmail.com>
2017-08-22 18:16:06 +0500

    Refactor scenes reducer file

--- chanqadim/reducers/scenes.js
+++ chanqadim/reducers/scenes.js
@@ -1,69 +1,83 @@
 import { combineReducers } from 'redux'
 
-const scenesInitialState = {
-  categories: { isFetching: true },
-  category: { isFetching: true },
-  bundle: { isFetching: true },
-  profile: { isFetching: true },
-  editProfile: { isFetching: false },
-  login: { isFetching: false },
-  add: { isFetching: true }
+function items (state = {}, action) {
+  return {
+    categories: categories(state.categories, action),
+    category: category(state.category, action),
+    bundle: bundle(state.bundle, action),
+    profile: profile(state.profile, action),
+    editProfile: editProfile(state.editProfile, action),
+    login: login(state.login, action),
+    add: add(state.add, action)
+  }
 }
 
-function items (state = scenesInitialState, action) {
+function categories (state = { isFetching: true }, action) {
+  return state
+}
+
+function category (state = { isFetching: true }, action) {
   switch (action.type) {
     case 'REQUEST_CATEGORY':
-      return {
-        ...state,
-        category: { isFetching: true, url: action.url }
-      }
+      return { isFetching: true, url: action.url }
     case 'RECEIVE_CATEGORY':
       return {
-        ...state,
-        category: { isFetching: false, url: action.items.entities.categories[action.items.result].url }
-      }
-    case 'REQUEST_BUNDLE':
-      return {
-        ...state,
-        bundle: { isFetching: true, url: action.url }
-      }
-    case 'RECEIVE_BUNDLE':
-      return {
-        ...state,
-        bundle: { isFetching: false, url: action.items.entities.bundles[action.items.result].url }
-      }
-    case 'RECEIVE_CURRENT_USER':
-      return {
-        ...state,
-        profile: { isFetching: false }
-      }
-    case 'REQUEST_CURRENT_USER':
-      return {
-        ...state,
-        profile: { isFetching: true }
-      }
-    case 'REQUEST_UPDATE_CURRENT_USER':
-      return {
-        ...state,
-        editProfile: { isFetching: true }
-      }
-    case 'RECEIVE_UPDATE_CURRENT_USER':
-      return {
-        ...state,
-        editProfile: { wasUpdateSuccessful: true, isFetching: false }
-      }
-    case 'REQUEST_LOGIN':
-      return {
-        ...state,
-        login: { isFetching: true }
-      }
-    case 'LOGIN_ERROR_RESPONSE':
-      return {
-        ...state,
-        login: { hasErrorHappened: true }
+        isFetching: false,
+        url: action.items.entities.categories[action.items.result].url
       }
   }
 
+  return state
+}
+
+function bundle (state = { isFetching: true }, action) {
+  switch (action.type) {
+    case 'REQUEST_BUNDLE':
+      return { isFetching: true, url: action.url }
+    case 'RECEIVE_BUNDLE':
+      return {
+        isFetching: false,
+        url: action.items.entities.bundles[action.items.result].url
+      }
+  }
+
+  return state
+}
+
+function profile (state = { isFetching: true }, action) {
+  switch (action.type) {
+    case 'RECEIVE_CURRENT_USER':
+      return { isFetching: false }
+    case 'REQUEST_CURRENT_USER':
+      return { isFetching: true }
+  }
+
+  return state
+}
+
+function editProfile (state = { isFetching: false }, action) {
+  switch (action.type) {
+    case 'REQUEST_UPDATE_CURRENT_USER':
+      return { isFetching: true }
+    case 'RECEIVE_UPDATE_CURRENT_USER':
+      return { wasUpdateSuccessful: true, isFetching: false }
+  }
+
+  return state
+}
+
+function login (state = { isFetching: false }, action) {
+  switch (action.type) {
+    case 'REQUEST_LOGIN':
+      return { isFetching: true }
+    case 'LOGIN_ERROR_RESPONSE':
+      return { hasErrorHappened: true }
+  }
+
+  return state
+}
+
+function add (state = { isFetching: true }, action) {
   return state
 }
 
